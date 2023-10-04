3a73dd6ed4a79304edbfec9b9aa4a69253bf2d44
NasirSTL <nasir@wustl.edu>
2023-10-04 17:11:26 -0500

    Flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,22 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		int canvasHeight = 512;
+		int canvasWidth = 22 * canvasHeight / 16;
+		StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+		
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		
+		StdDraw.setPenColor(70, 100, 90);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 30, 30, 3);
+		StdDraw.filledRectangle(16, 3, 13, 3);
+		StdDraw.filledRectangle(6, 13, 16, 3);
+		
+		//StdDraw.filledRectangle(11, 8, 11, 1);
+		//StdDraw.filledRectangle(8, 8, 1, 8);
+		
+	
 	}
 }
